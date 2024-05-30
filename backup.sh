5e87dd2aba116fe2efb2a77a7d508a92989d3402
rokujuku <95484647+rokujuku@users.noreply.github.com>
2024-05-30 17:51:27 +0400

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ mkdir /var/backup/$data
 cp -r /etc/frr /var/backup/$data
 cp -r /etc/dhcp /var/backup/$data
 cp -r /etc/NetworkManager/system-connections /var/backup/$data
-cp -r /etc/nftables /var/backup/$data
+cp -r /etc/nftables.conf /var/backup/$data
 cd /var/backup
 tar czfv "./$data.tar.gz" ./$data
 rm -r /var/backup/$data
